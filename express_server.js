47fc93cde2ef66146692be8d8bad2e08bc378ea1
Zeyu <haozai_03@hotmail.com>
2019-03-12 18:48:01 +0000

    display url

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+
 var PORT = 8080; // default port 8080
+app.set("view engine", "ejs")
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,8 +17,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
