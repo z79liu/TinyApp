b82c8905773c3c0de3c1e92b329680dded361a87
Zeyu <haozai_03@hotmail.com>
2019-03-15 21:49:39 +0000

    project complete

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
+//declaring and setting up the enviroment/ packages for the program
+
 var express = require("express");
 var cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
-
 var app = express();
 app.use(cookieSession({
   name: 'session',
@@ -12,6 +13,7 @@ app.use(cookieSession({
 
 var PORT = 8080; // default port 8080
 app.set("view engine", "ejs")
+
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
@@ -30,6 +32,8 @@ const users = {
 }
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -37,17 +41,17 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//Test if the user cookie matches the current cookie, so that it
+// only shows URLS that belong to that cookie. Passes the data to urls_index template
+//
 
 app.get("/urls", (req, res) => {
-  console.log('cuurent cookie is', req.session.userId )
-  var filtedList = {}
+  var filtedList = {} ;
   for (shortUrl in urlDatabase) {
     if (urlDatabase[shortUrl].userID == req.session.userId ){
       filtedList[shortUrl] = urlDatabase[shortUrl].longURL;
-      }
     }
-  console.log('this is the urldatabase:', urlDatabase)
-  console.log('this is filtlist:', filtedList)
+  }
   let templateVars = { urls: filtedList,
     user: users[req.session.userId],
     error: "please login first"
@@ -59,17 +63,21 @@ app.get("/about", (req, res) => {
   res.send("Welcome to Zeyu Liu's URL shortner");
 });
 
+//if user is not logged in / no cookie active it will be redirected
+//to login
 
 app.get("/urls/new", (req, res) => {
   if (req.session.userId === undefined) {
-    res.redirect("/login")
+    res.redirect("/login") ;
   } else {
   let templateVars = { urls: urlDatabase,
     user: users[req.session.userId]
   };
   res.render("urls_new", templateVars);
-}
+    }
 });
+
+
 app.get("/urls/:shortURL", (req, res) => {
   if (req.session.userId === undefined) {
     res.redirect("/login")
@@ -82,22 +90,25 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 }
 });
-// const urlDatabase = {
-//   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-//   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
-// };
+
+
+//generate a random shortURL which will be stored in the Database
+
 app.post("/urls", (req, res) => {
   var short = generateRandomString()
   urlDatabase[short] = {longURL: req.body.longURL, userID: req.session.userId};
    // Log the POST request body to the console
   res.redirect(`/urls/${short}`);         // Respond with 'Ok' (we will replace this)
 });
+
+
 //takes user to the actual long URL from the shortURl
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
   const longURL = urlDatabase[shortURL].longURL
   res.redirect(longURL);
 });
+
 //added delete button and redirect user back to the home page
 app.post("/urls/:shortURL/delete",(req, res) =>{
   const shortURL = req.params.shortURL
@@ -108,6 +119,8 @@ app.post("/urls/:shortURL/delete",(req, res) =>{
   delete urlDatabase[shortURL]
   res.redirect("/urls")
 })
+
+
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
   urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.session.userId}
@@ -119,7 +132,10 @@ app.get("/login", (req, res) => {
   };
   res.render("urls_login", templateVars);
 });
-//for the login button
+
+
+//for the login button, if email does not exist, send erro 403 code
+// if it does exist match the password/ email and then redirect to urls page
 app.post("/login", (req, res) => {
   if (!dupEmail(req.body.email)) {
     res.status(403).send("This mail does not exist in the system");
@@ -137,16 +153,22 @@ app.post("/login", (req, res) => {
     }
   }
 });
+
+//logout will clear the current active cookie
 app.post("/logout", (req, res) => {
   req.session.userId = null;
   res.redirect("/urls")
 });
+
+//creates cookie when user first login and render the urls_register page
 app.get("/register", (req, res) => {
   let templateVars = {user: users[req.session.userId],
                       error: undefined,
                      };
   res.render("urls_register", templateVars)
 });
+
+//helper function to check for dupliate emails
 function dupEmail(emails){
   for (person in users){
     if (users[person].email === emails){
@@ -154,6 +176,8 @@ function dupEmail(emails){
     }
   }
 }
+
+//post register request will check if email already exist and make sure the fields are inputed
 app.post("/register", (req, res) => {
   if (!req.body.email || !req.body.password ) {
     res.render("urls_register",{ error: 'Please fill in all input fields'})
@@ -171,6 +195,8 @@ app.post("/register", (req, res) => {
     res.redirect("/urls")
   }
 });
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
